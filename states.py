36eb178daf1328d3f2a262718625eba0b84e1c6d
FunyaMain <funyamain@gmail.com>
2026-04-29 13:58:53 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,1 +1,14 @@
+from aiogram.fsm.state import StatesGroup, State
 
+class Registration(StatesGroup):
+    name = State()
+    age = State()
+    city = State()
+    gender = State()
+    search = State()
+    about = State()
+    photo = State()
+    confirm = State()
+
+class Anon(StatesGroup):
+    text = State()

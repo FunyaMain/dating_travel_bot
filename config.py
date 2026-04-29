ba7bb045627fdff1fb07299db53e9fbac4816333
FunyaMain <funyamain@gmail.com>
2026-04-29 13:57:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,16 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Каналы
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+CHAT_ID = int(os.getenv("CHAT_ID"))
+
+# Модерация
+MOD_CHAT_ID = int(os.getenv("MOD_CHAT_ID"))
+
+# БД (Railway PostgreSQL)
+DB_URL = os.getenv("DATABASE_URL")

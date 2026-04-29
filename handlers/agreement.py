a7d7a550900a99d75e0819796b7f37515d54224c
FunyaMain <funyamain@gmail.com>
2026-04-29 14:07:59 +0300

    Update agreement.py

--- handlers/agreement.py
+++ handlers/agreement.py
@@ -1,1 +1,11 @@
+from aiogram import Router, F
+from aiogram.types import Message
 
+router = Router()
+
+@router.message(F.text == "📜 О нас / Соглашение")
+async def agreement(message: Message):
+    await message.answer(
+        "📜 Соглашение:\n\n"
+        "Используя бота, вы принимаете правила сервиса."
+    )

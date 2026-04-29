61cd5dac9bef96cc4b2f76d6c4509cb39c19891e
FunyaMain <funyamain@gmail.com>
2026-04-29 13:59:11 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,16 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="👤 Мой профиль")],
+        [KeyboardButton(text="💌 Анонимное сообщение")],
+        [KeyboardButton(text="📜 О нас / Соглашение")],
+        [KeyboardButton(text="💎 Поддержать")]
+    ],
+    resize_keyboard=True
+)
+
+confirm_kb = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="✅ Подтвердить", callback_data="confirm")],
+    [InlineKeyboardButton(text="✏️ Редактировать", callback_data="edit")]
+])

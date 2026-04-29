4e9a652d63a6f021af9dc59a7933d79ce04a83e9
FunyaMain <funyamain@gmail.com>
2026-04-29 14:00:54 +0300

    Update registration.py

--- handlers/registration.py
+++ handlers/registration.py
@@ -1,1 +1,13 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from states import Registration
+from keyboards import confirm_kb
 
+router = Router()
+
+@router.message(Registration.name)
+async def set_name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Registration.age)
+    await message.answer("Возраст?")

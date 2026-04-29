68f712d352eb151cd1f74b08e762fe1101f59507
FunyaMain <funyamain@gmail.com>
2026-04-29 14:15:21 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,1 +1,8 @@
+from aiogram import Router, F
+from aiogram.types import Message
 
+router = Router()
+
+@router.message(F.text == "👤 Мой профиль")
+async def profile(message: Message):
+    await message.answer("Тут будет твой профиль")
